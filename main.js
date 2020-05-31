01e2412204369f75dc606dbb64817c4e5f8991b4
Jayesh <jayeshthakur47@gmail.com>
2020-05-31 16:08:14 +0530

    Done basic UI started working on MC movement

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+// MC here 
+var MC = {
+    x : 0,
+    y : 0,
+    size : 50
+}
+
+$(document).ready(function(){
+    $("#gameBody").on("click",updatePostion)
+});
+
+function updatePostion()
+{
+    MC.x = event.pageX;
+    MC.y = event.pageY;
+    console.info("MC Current position :: X : "+ MC.x + "Y : "+ MC.y);
+}
+
+
